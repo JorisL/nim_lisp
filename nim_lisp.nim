09940e8cdd9765fc97041ed4a24b06bdd0bd217a
JorisL <JorisL@users.noreply.github.com>
2023-02-21 23:50:41 +0100

    environment scoping seems to be fixed (and neater now it is pass by reference) implemented several functions, making it posible to define a repl in the lisp language

--- nim_lisp.nim
+++ nim_lisp.nim
@@ -7,7 +7,7 @@ from std/sequtils import zip
 from math import sum, prod
 from strutils import parseFloat, strip, startsWith, join
 
-let primitives = ["+", "-", "*", "/", "=", "<", "do", "list", "define", "lambda", "eval", "while", "cond"]
+let primitives = ["+", "-", "*", "/", "==", "<", "do", "list", "define", "lambda", "eval", "while", "cond", "input", "parse", "print"]
 
 
 type
@@ -29,15 +29,84 @@ type
           code: Node
         of dt_macro: macro_var: string
         of dt_error: error_var: string
-    
-    Environment = ref object
+
+    Environment = object
         t: Table[string, Node]
-        has_parent: bool
-        parent_environment: Environment
+        has_parent_env: bool
+        parent_env: ref Environment
 
-# TODO: convert dataflow-style environment passing into a global stack-based environment
 
-var base_env = new(Environment)
+proc new_num_node(num: float): Node =
+    return Node(dataType: dt_number, number_var: num)
+
+proc new_string_node(text: string): Node =
+    return Node(dataType: dt_string, string_var: text)
+
+proc new_symbol_node(symbol: string): Node =
+    return Node(dataType: dt_symbol, symbol_var: symbol)
+
+proc new_list_node(data: seq[Node]): Node =
+    return Node(dataType: dt_lst, list_var: data)
+
+proc new_nil_node(): Node =
+    return Node(dataType: dt_nil)
+
+proc new_error_node(error_message: string): Node =
+    return Node(dataType: dt_error, error_var: error_message)
+
+
+proc `==`(n1, n2: Node): bool =
+    result = n1.dataType == n2.dataType
+    if result:
+        case n1.dataType
+        of dt_number:
+            result = n1.number_var == n2.number_var
+        of dt_string:
+            result = n1.string_var == n2.string_var
+        of dt_symbol:
+            result = n1.symbol_var == n2.symbol_var
+        of dt_nil:
+            result = true # nil == nil -> true
+        of dt_lst:
+            assert 1 == 0 # TODO
+        else:
+            assert 1 == 0 # TODO
+
+
+proc to_string(self: Node): string =
+    case self.dataType
+    of dt_number:
+        return fmt"{self.number_var}"
+    of dt_string:
+        return "\"" & fmt"{self.string_var}" & "\""
+    of dt_symbol:
+        return self.symbol_var
+    of dt_nil:
+        return "nil"
+    of dt_error:
+        return fmt"Error: {self.error_var}"
+    else:
+        return fmt"Error: printing for data of type {self.dataType} not implemented."
+
+
+proc set(self: ref Environment, name: string, value: Node) =
+    self.t[name] = value
+
+proc get(self: ref Environment, name: string): Node =
+    try:
+        return self.t[name]
+    except KeyError:
+        if self.has_parent_env:
+            return self.parent_env.get(name)
+        else:
+            return new_error_node(fmt"{name} not found in environment")
+
+proc new_subenv(parent_env: ref Environment): ref Environment =
+    var subenv: ref Environment
+    new(subenv)
+    subenv.has_parent_env = true
+    subenv.parent_env = parent_env
+    return subenv
 
 
 proc isValidNumber(str: string): bool =
@@ -59,33 +128,10 @@ proc getTokenType(token: string): DataType =
         result = dt_symbol
 
 
-#let source_in: string = "(do (define foo 6) (* foo 7))"
-#let source_in: string = "((lambda (x) (* x x)) 5)"
-let source_in: string = "(do (define x 42) ((lambda (x) (* x x)) 5) x)"
-
-
 proc read_tokens(source_in: string): Deque[string] =
     var tokens = collect(newSeq):
         for token in findAll(source_in, re"""\s*([\()]|"(?:\\.|[^\\"])*"?|;.*|[^\s()";]*)\s*"""): strip(token)
     return tokens.toDeque()
-
-var tokens = read_tokens(source_in)
-
-
-proc new_num_node(num: float): Node =
-    return Node(dataType: dt_number, number_var: num)
-
-proc new_symbol_node(symbol: string): Node =
-    return Node(dataType: dt_symbol, symbol_var: symbol)
-
-proc new_list_node(data: seq[Node]): Node =
-    return Node(dataType: dt_lst, list_var: data)
-
-proc new_nil_node(): Node =
-    return Node(dataType: dt_nil)
-
-proc new_error_node(error_message: string): Node =
-    return Node(dataType: dt_error, error_var: error_message)
 
 
 proc genAbstractSyntaxTree(tokens: var Deque[string]): Node =
@@ -126,6 +172,11 @@ proc genAbstractSyntaxTree(tokens: var Deque[string]): Node =
             return new_error_node("yada")
 
 
+proc read(source_in: string): Node =
+    var tokens = read_tokens(source_in)
+    return genAbstractSyntaxTree(tokens)
+
+
 proc toJsonString(n: Node): string =
     var dataType = n.dataType
     case dataType
@@ -155,35 +206,6 @@ proc toPlantUmlString(n: Node): string =
     "@startjson\n" & toJsonString(n) & "\n@endjson"
 
 
-var ast = genAbstractSyntaxTree(tokens)
-
-echo toPlantUmlString(ast)
-
-
-#proc eval(n: Node, env: Environment): (Node, Environment)
-
-proc subenv_create(parent_env: Environment): Environment =
-    return Environment(has_parent: true, parent_environment: parent_env)
-
-proc subenv_delete(subenv: Environment): Environment =
-    assert subenv.has_parent
-    return subenv.parent_environment
-
-proc env_write(env: Environment, symbol_name: string, value: Node): Environment =
-    result = env
-    result.t[symbol_name] = value
-
-# TODO: use string instead of symbol_node?
-proc env_read(env: Environment, symbol_name: string): Node =
-    # TODO: also try reading from parent environment
-    try:
-        return env.t[symbol_name]
-    except KeyError:
-        if env.has_parent:
-            return env_read(env.parent_environment, symbol_name)
-        else:
-            return new_error_node(fmt"{symbol_name} not found in environment.")
-
 proc eval_sum(nodes: seq[Node]): Node =
     return new_num_node(sum(block: collect(newSeq): (for n in nodes: n.number_var)))
 
@@ -198,12 +220,13 @@ proc eval_divide(nodes: seq[Node]): Node =
     # TODO: what about more than 2 arguments?
     return new_num_node(nodes[0].number_var / nodes[1].number_var)
 
-#proc eval_equals(nodes: seq[Node]): Node =
-#    # TODO: handle more cases
-#    if (nodes[0].dataType == nodes[1].dataType) and (nodes[0].str == nodes[1].str) and nodes[0].num == nodes[1].num:
-#        return new_symbol_node("#t")
-#    else:
-#        return new_nil_node()
+proc eval_equals(nodes: seq[Node]): Node =
+    # TODO: handle more cases
+    assert len(nodes) == 2
+    if nodes[0] == nodes[1]:
+        return new_symbol_node("#t")
+    else:
+        return new_nil_node()
 
 proc eval_lt(nodes: seq[Node]): Node =
     # TODO: error handling, should exactly be 2 arguments, and both numbers
@@ -215,13 +238,12 @@ proc eval_lt(nodes: seq[Node]): Node =
 # TODO: eval_buildin_function?
 
 
-proc eval(n: Node, env: Environment): (Node, Environment) =
-    var env = env # TODO: clean up, only define where env is modified?
+proc eval(n: Node, env: ref Environment): Node =
     var dataType = n.dataType
     var result_node: Node
     case dataType
     of dt_lst:
-        var (first_node_result, env) = eval(n.list_var[0], env)
+        var first_node_result = eval(n.list_var[0], env)
         var first_node_result_datatype = first_node_result.dataType # TODO: simplify?
         case first_node_result_datatype
         of dt_primitive:
@@ -229,40 +251,40 @@ proc eval(n: Node, env: Environment): (Node, Environment) =
             var nodes = n.list_var[1 .. ^1]
             case primitive:
             of "+":
-                result_node = eval_sum(block: collect(newSeq): (for n in nodes: eval(n,  env)[0]))
+                result_node = eval_sum(block: collect(newSeq): (for n in nodes: eval(n,  env)))
             of "-":
-                result_node = eval_subtract(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
+                result_node = eval_subtract(block: collect(newSeq): (for n in nodes: eval(n, env)))
             of "*":
-                result_node = eval_product(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
+                result_node = eval_product(block: collect(newSeq): (for n in nodes: eval(n, env)))
             of "/":
-                result_node = eval_divide(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
-#            of "=":
-#                result_node = eval_equals(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
+                result_node = eval_divide(block: collect(newSeq): (for n in nodes: eval(n, env)))
+            of "==":
+                result_node = eval_equals(block: collect(newSeq): (for n in nodes: eval(n, env)))
             of "<":
-                result_node = eval_lt(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
+                result_node = eval_lt(block: collect(newSeq): (for n in nodes: eval(n, env)))
             of "do":
                 # evaluate all arguments, and return the (evaluated) last one
                 # TODO: pass previous environment through each cycle
                 var tmp = collect(newSeq): (for n in nodes: eval(n, env))
-                result_node = tmp[^1][0]
+                result_node = tmp[^1]
             of "list":
-                result_node = new_list_node(block: collect(newSeq): (for n in nodes: eval(n, env)[0]))
+                result_node = new_list_node(block: collect(newSeq): (for n in nodes: eval(n, env)))
             of "define":
                 # TODO: check that frist argument of define is a symbol name
-                var (tmp_n, env) = eval(nodes[1], env)
-                env = env_write(env, nodes[0].symbol_var, tmp_n)
+                var tmp_n = eval(nodes[1], env)
+                env.set(nodes[0].symbol_var, tmp_n)
                 result_node = nodes[0]
             of "eval":
                 # evaluate a list as code. first run eval on the first argument to get the (quoted) list,
                 # and then perform eval on that returned list to evaluate the contents.
-                var (tmp_n, env) = eval(nodes[0], env)
-                (result_node, env) = eval(tmp_n, env)
+                var tmp_n = eval(nodes[0], env)
+                result_node = eval(tmp_n, env)
             of "while":
                 # (while cond expr)
                 while true:
-                    var (tmp_n, env) = eval(nodes[0], env)
+                    var tmp_n = eval(nodes[0], env)
                     if tmp_n.dataType != dt_nil:
-                        (tmp_n, env) = eval(nodes[1], env) # TODO: discard result?
+                        tmp_n = eval(nodes[1], env) # TODO: discard result?
                     else:
                         break
                 result_node = new_nil_node() # TODO: what to return?
@@ -271,16 +293,26 @@ proc eval(n: Node, env: Environment): (Node, Environment) =
                 # to last until test_i evaluates non-false and then returns expr_i
                 result_node = new_nil_node()
                 for i in 0 ..< len(nodes):
-                    var (tmp_n, env) = eval(nodes[i].list_var[0], env)
+                    var tmp_n = eval(nodes[i].list_var[0], env)
                     if tmp_n.dataType != dt_nil:
-                        (result_node, env) = eval(nodes[i].list_var[1], env)
+                        result_node = eval(nodes[i].list_var[1], env)
                         break
             of "lambda":
                 # (lambda (arg1 arg2 argn) code)
                 result_node = Node(dataType: dt_lambda)
                 result_node.argument_names = collect(newSeq): (for n in nodes[0].list_var: n.symbol_var)
                 result_node.code = nodes[1]
+            of "input":
+                # read from stdin
+                result_node = new_string_node(readLine(stdin))
+            of "parse":
+                # eval argument and parse text into an AST
+                result_node = read(eval(nodes[0], env).string_var)
+            of "print":
+                # eval argument and print text to the stdout
+                echo to_string(eval(nodes[0], env))
             else:
+                assert 1 == 0 # TODO
                 result_node = nodes[0]
         of dt_symbol:
             result_node = first_node_result # TODO
@@ -288,25 +320,38 @@ proc eval(n: Node, env: Environment): (Node, Environment) =
             var arguments = n.list_var[1 .. ^1]
             assert (len(arguments) == len(first_node_result.argument_names)) # todo: convert to generate error node
             # evaluate each argument, and write it with it's corresponding argument name to a new sub-environment
-            
+            var subenv = new_subenv(env)
             for (argument_name, argument) in zip(first_node_result.argument_names, arguments):
-                var (tmp_n, env) = eval(argument, env)
-                env = env_write(env, argument_name, tmp_n)
-
-            (result_node, env) = eval(first_node_result.code, subenv_create(env))
-            env = subenv_delete(env)
+                var tmp_n = eval(argument, env) # TODO: evaluate in env or subenv?
+                subenv.set(argument_name, tmp_n)
+            result_node = eval(first_node_result.code, subenv)
         of dt_macro:
             result_node = first_node_result # TODO
         else:
             result_node = new_error_node("First element in a list should be a closure, primitive, or symbol after evaluation.")
     of dt_symbol:
-        result_node = env_read(env, n.symbol_var)
+        result_node = env.get(n.symbol_var)
     else:
         result_node = n
-    return(result_node, env)
+    return result_node
 
 
+var env = new(Environment)
+env.has_parent_env = false
 
-echo ""
-let (ast_result, env_result) = eval(ast, base_env)
+#let source_in: string = "(do (define foo 6) (* foo 7))"
+#let source_in: string = "((lambda (x) (* x x)) 5)"
+#let source_in: string = "(do (define x 42) ((lambda (x) (* x x)) 5) x)"
+#let source_in: string = "(input)"
+#let source_in: string = "(do (define x (input)) x)"
+#let source_in: string = """(do (eval (parse "(+ 1 2)")))"""
+#let source_in: string = "(parse (input))"
+let source_in: string = "(while 1 (print (eval (parse (input)))))"
+
+var ast = read(source_in)
+#echo toPlantUmlString(ast)
+#echo ""
+
+let ast_result = eval(ast, env)
+
 echo toPlantUmlString(ast_result)
